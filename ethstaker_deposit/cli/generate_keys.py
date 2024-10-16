d85939521b0b2351d47753afaefaf60586c0bbe1
uink45 <79078981+uink45@users.noreply.github.com>
2024-10-16 22:10:15 +1000

    Update generate_keys.py

--- ethstaker_deposit/cli/generate_keys.py
+++ ethstaker_deposit/cli/generate_keys.py
@@ -94,20 +94,6 @@ def generate_keys_arguments_decorator(function: Callable[..., Any]) -> Callable[
             prompt=False,  # the callback handles the prompt
         ),
         jit_option(
-            callback=captive_prompt_callback(
-                lambda address: validate_withdrawal_address(None, None, address),
-                lambda: load_text(['arg_withdrawal_address', 'prompt'], func='generate_keys_arguments_decorator'),
-                lambda: load_text(['arg_withdrawal_address', 'confirm'], func='generate_keys_arguments_decorator'),
-                lambda: load_text(['arg_withdrawal_address', 'mismatch'], func='generate_keys_arguments_decorator'),
-                default="",
-                prompt_if_none=True,
-            ),
-            default="",
-            help=lambda: load_text(['arg_withdrawal_address', 'help'], func='generate_keys_arguments_decorator'),
-            param_decls=['--withdrawal_address', '--execution_address', '--eth1_withdrawal_address'],
-            prompt=False,  # the callback handles the prompt
-        ),
-        jit_option(
             default=False,
             is_flag=True,
             param_decls='--pbkdf2',
@@ -129,8 +115,7 @@ def generate_keys_arguments_decorator(function: Callable[..., Any]) -> Callable[
 @click.command()
 @click.pass_context
 def generate_keys(ctx: click.Context, validator_start_index: int,
-                  num_validators: int, folder: str, chain: str, keystore_password: str,
-                  withdrawal_address: HexAddress, pbkdf2: bool,
+                  num_validators: int, folder: str, chain: str, keystore_password: str, pbkdf2: bool,
                   devnet_chain_setting: Optional[BaseChainSetting], **kwargs: Any) -> None:
     mnemonic = ctx.obj['mnemonic']
     mnemonic_password = ctx.obj['mnemonic_password']
@@ -152,18 +137,18 @@ def generate_keys(ctx: click.Context, validator_start_index: int,
         amounts=amounts,
         chain_setting=chain_setting,
         start_index=validator_start_index,
-        hex_withdrawal_address=withdrawal_address,
+        hex_withdrawal_address=None,
         use_pbkdf2=pbkdf2
     )
 
     timestamp = time.time()
 
     keystore_filefolders = credentials.export_keystores(password=keystore_password, folder=folder, timestamp=timestamp)
-    deposits_file = credentials.export_deposit_data_json(folder=folder, timestamp=timestamp)
+    # deposits_file = credentials.export_deposit_data_json(folder=folder, timestamp=timestamp)
     if not credentials.verify_keystores(keystore_filefolders=keystore_filefolders, password=keystore_password):
         raise ValidationError(load_text(['err_verify_keystores']))
-    if not verify_deposit_data_json(deposits_file, credentials.credentials):
-        raise ValidationError(load_text(['err_verify_deposit']))
+    # if not verify_deposit_data_json(deposits_file, credentials.credentials):
+    #     raise ValidationError(load_text(['err_verify_deposit']))
     click.echo(load_text(['msg_creation_success']) + folder)
     if not config.non_interactive:
         click.pause(load_text(['msg_pause']))
